cae3b7e2099892b50e0a0682fce0454a302e8a46
MAV3R1CK <eddymav247@gmail.com>
2022-10-31 15:15:39 +0100

    mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -17,40 +17,40 @@ function App() {
           </h2>
           {/* <p id="slack">Maverick</p> */}
         </div>
-        <div className="mt-4 w-[70vW] flex flex-col items-center">
-          <a className="my-4 bg-[#c8cdd7] w-full text-center p-6 rounded-md">
+        <div className="mt-4 w-[85vW] sm:w-[70vW] flex flex-col items-center">
+          <a className="my-4 bg-[#c8cdd7] w-full text-center p-6 rounded-md text-xl">
             Twitter Link
           </a>
           <a
-            className="my-4 bg-[#c8cdd7] w-full text-center p-6 rounded-md"
+            className="text-xl my-4 bg-[#c8cdd7] w-full text-center p-6 rounded-md"
             href="https://training.zuri.team/"
             id="btn__zuri"
           >
             Zuri Team
           </a>
           <a
-            className="my-4 bg-[#c8cdd7] w-full text-center p-6 rounded-md"
+            className="text-xl my-4 bg-[#c8cdd7] w-full text-center p-6 rounded-md"
             href="https://books.zuri.team/"
             id="books"
           >
             Zuri Books
           </a>
           <a
-            className="my-4 bg-[#c8cdd7] w-full text-center p-6 rounded-md"
+            className="text-xl my-4 bg-[#c8cdd7] w-full text-center p-6 rounded-md"
             href="https://books.zuri.team/python-for-beginners?ref_id=maverick"
             id="book__python"
           >
             Python Books
           </a>
           <a
-            className="my-4 bg-[#c8cdd7] w-full text-center p-6 rounded-md"
+            className="my-4 bg-[#c8cdd7] w-full text-center p-6 rounded-md text-xl"
             href="https://background.zuri.team"
             id="pitch"
           >
             Background Check for Coders
           </a>
           <a
-            className="my-4 bg-[#c8cdd7] w-full text-center p-6 rounded-md"
+            className="my-4 bg-[#c8cdd7] w-full text-center p-6 rounded-md text-xl"
             href="https://books.zuri.team/design-rules"
             id="book__design"
           >
@@ -61,10 +61,10 @@ function App() {
             <img src={Git} className="w-12 h-12 ml-2" />
           </div>
         </div>
-        <div className="py-4 px-12 my-6 mt-8 w-[90vW] flex flex-row items-center justify-between">
+        <div className="py-2 sm:py-4 px-4 sm:px-12 my-2 sm:my-6 mt-8 w-[90vW] flex sm:flex-row flex-col justify-between">
           <h1 className="font-bold text-[25px]">Zuri <span className="text-[40px] text-red-500">.</span> Internship</h1>
           <p className="text-xs text-gray-500">HNG internship 9 frontend task</p>
-          <img src={I4G} />
+          <img src={I4G} className="self-left h-[55px] w-[300px] mt-[20px]"/>
         </div>
       </div>
     </div>
