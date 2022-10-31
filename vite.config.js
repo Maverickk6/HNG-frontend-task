ab7be3ca376b7ef0db88cbf31f94fb42b46a9b4a
MAV3R1CK <eddymav247@gmail.com>
2022-10-31 23:41:55 +0100

    resolved gh pages issue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/maverickk6/',
+  base: '/HNG-frontend-task/',
   plugins: [react()]
 })
