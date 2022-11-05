04dca8711e5279d73d97819e2abe43f6f0fc9e44
MAV3R1CK <eddymav247@gmail.com>
2022-11-05 10:26:41 +0100

    deploy fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { createBrowserRouter, HashRouter, RouterProvider, Route } from "react-router-dom";
 import ContactPage from "./pages/ContactPage";
 
 const router = createBrowserRouter([
