41cfa85c4eaefcc025e1ed795041a3c2bc3a5db6
MAV3R1CK <eddymav247@gmail.com>
2022-11-07 10:22:17 +0100

    UI: error page ui responsive improvement

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -7,15 +7,15 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div className="h-full w-full flex flex-col px-20 pt-20 pb-4 justify-center">
+    <div className="h-full w-full flex flex-col px-4 sm:px-20 pt-20 pb-4 justify-center">
       <div
         id="error_page"
-        className="w-[45vW] h-[50vH] rounded-md p-4 mb-12"
+        className="w-[90vW] sm:w-[50vW] h-[50vH] rounded-md p-4 mb-12"
       >
         <p className="text-xs text-blue-500 mb-4">404 error</p>
-        <h1 className="text-[50px] leading-8 mb-10 mt-8">We can't find that page</h1>
+        <h1 className="text-[50px] leading-[50px] mb-10 mt-8">We can't find that page</h1>
         <p className="mb-10 font-light text-[16px]">Sorry, the page you are looking for doesn't exist.</p>
-        <div className="flex flex-row justify-between w-[50%]">
+        <div className="flex flex-row justify-between sm:w-[90%]">
           <Link className="px-[15px] py-2 border border-gray-300 rounded-md flex flex-row items-center" to={'/contact'}><img src={Left} className="h-[10px] w-[10px] ml-[5px] mr-[10px]"/>Go back</Link>
           <Link className="p-2 bg-blue-500 rounded-md text-white" to={'/'}>Take me home</Link>
         </div>
