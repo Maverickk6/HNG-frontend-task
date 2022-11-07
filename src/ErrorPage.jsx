8c9d44003a3d4c07dc7fba5bab709d2d6a465fd2
MAV3R1CK <eddymav247@gmail.com>
2022-11-07 10:35:40 +0100

    UI: error page ui responsive improvement

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,5 +1,5 @@
 import { useRouteError, Link } from "react-router-dom";
-import Left from './assets/arrow-left.png'
+import Left from "./assets/arrow-left.png";
 import Footer from "./components/Footer";
 
 export default function ErrorPage() {
@@ -13,15 +13,26 @@ export default function ErrorPage() {
         className="w-[90vW] sm:w-[50vW] h-[50vH] rounded-md p-4 mb-12"
       >
         <p className="text-xs text-blue-500 mb-4">404 error</p>
-        <h1 className="text-[50px] leading-[50px] mb-10 mt-8">We can't find that page</h1>
-        <p className="mb-10 font-light text-[16px]">Sorry, the page you are looking for doesn't exist.</p>
+        <h1 className="text-[50px] leading-[50px] mb-10 mt-8">
+          We can't find that page
+        </h1>
+        <p className="mb-10 font-light text-[16px]">
+          Sorry, the page you are looking for doesn't exist.
+        </p>
         <div className="flex flex-row justify-between sm:w-[90%]">
-          <Link className="px-[15px] py-2 border border-gray-300 rounded-md flex flex-row items-center" to={'/contact'}><img src={Left} className="h-[10px] w-[10px] ml-[5px] mr-[10px]"/>Go back</Link>
-          <Link className="p-2 bg-blue-500 rounded-md text-white" to={'/'}>Take me home</Link>
+          <Link
+            className="px-[15px] py-2 border border-gray-300 rounded-md flex flex-row items-center"
+            to={"/contact"}
+          >
+            <img src={Left} className="h-[10px] w-[10px] ml-[5px] mr-[10px]" />
+            Go back
+          </Link>
+          <Link className="p-2 bg-blue-500 rounded-md text-white" to={"/"}>
+            Take me home
+          </Link>
         </div>
       </div>
       <Footer />
     </div>
   );
 }
-
