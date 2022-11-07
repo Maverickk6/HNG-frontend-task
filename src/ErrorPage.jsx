e7dc0a6ade023afd9f88052bd72bba634ac5ba92
MAV3R1CK <eddymav247@gmail.com>
2022-11-07 09:25:07 +0100

    error UI

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,21 +1,25 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, Link } from "react-router-dom";
+import Left from './assets/arrow-left.png'
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div className="h-full w-full flex items-center justify-center p-20 ">
+    <div className="h-full w-full flex flex-col p-20 justify-center">
       <div
-        id="error-page"
-        className="w-[40vW] h-[40vH] bg-gray-300 rounded-md p-4 self-center"
+        id="error_page"
+        className="w-[45vW] h-[50vH] rounded-md p-4"
       >
-        <h1 className="mb-2 text-2xl">Oops!</h1>
-        <p className="mb-8">Sorry, an unexpected error has occurred. </p>
-        <p>
-          <i>{error.statusText || error.message}</i>
-        </p>
+        <p className="text-xs text-blue-500 mb-4">404 error</p>
+        <h1 className="text-[50px] leading-8 mb-12 mt-8">We can't find that page</h1>
+        <p className="mb-12 font-light text-[16px]">Sorry, the page you are looking for doesn't exist.</p>
+        <div className="flex flex-row justify-between w-[50%]">
+          <Link className="px-[15px] py-2 border border-gray-300 rounded-md flex flex-row items-center" to={'/contact'}><img src={Left} className="h-[10px] w-[10px] ml-[5px] mr-[10px]"/>Go back</Link>
+          <Link className="p-2 bg-blue-500 rounded-md text-white" to={'/'}>Take me home</Link>
+        </div>
       </div>
     </div>
   );
 }
+
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,6 +1,26 @@
 import Footer from "../components/Footer";
+import { Formik, Form, useField, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 function ContactPage() {
+  const initialValues = {
+    first_name: "",
+    last_name: "",
+    email: "",
+    message: "",
+    checkbox: false,
+  };
+
+  const validationSchema = Yup.object().shape({
+    first_name: Yup.string().required("Please enter your first name"),
+    last_name: Yup.string().required("Please enter your last name"),
+    email: Yup.string().required("Email is required"),
+    message: Yup.string().required("Please enter a message"),
+    checkbox: Yup.string().required("Please select the checkbox"),
+  });
+
+  const onSubmit = () => alert('Submitted');
+
   return (
     <div className="h-[full] w-[100%] flex flex-col items-center pb-6 md:pb-1">
       <div className="flex flex-col px-2 h-[65%] w-[90%] md:w-[60%] mx-auto mb-4 sm:mb-8 mt-16 rounded">
@@ -10,73 +30,91 @@ function ContactPage() {
         <p className="font-Inter font-light text-lg mb-16 text-[#475467]">
           Hi there, contact me to ask me about anything you have in mind.
         </p>
-        <form className="flex flex-col ">
-          <div className="flex flex-col sm:flex-row sm:items-center justify-between mb-4 sm:mb-6">
-            <div className="flex flex-col mb-6 sm:mb-2 sm:w-[45%]">
-              <label htmlFor="first_name" className="text-[#344054] text-sm mb-1">
-                First name
+        <Formik
+          initialValues={initialValues}
+          onSubmit={onSubmit}
+          validationSchema={validationSchema}
+        >
+          <Form className="flex flex-col ">
+            <div className="flex flex-col sm:flex-row sm:items-center justify-between mb-4 sm:mb-6">
+              <div className="flex flex-col mb-6 sm:mb-2 sm:w-[45%]">
+                <label
+                  htmlFor="first_name"
+                  className="text-[#344054] text-sm mb-1"
+                >
+                  First name
+                </label>
+                <Field
+                  name="first_name"
+                  required
+                  id="first_name"
+                  placeholder="Enter your first name"
+                  className="placeholder:text-[#667085] placeholder:text-[15px] border border-[#D0D5DD] py-2 px-4 rounded-lg hover:border-[#1570EF] focus:outline-[#84CAFF] focus:shadow-outline"
+                />
+              </div>
+              <div className="flex flex-col mb-4 sm:mb-2 sm:w-[45%]">
+                <label
+                  htmlFor="last_name"
+                  className="text-[#344054] text-sm mb-1"
+                >
+                  Last name
+                </label>
+                <Field
+                  name="last_name"
+                  required
+                  id="last_name"
+                  placeholder="Enter your last name"
+                  className="placeholder:text-[#667085] placeholder:text-[15px] border border-[#D0D5DD] py-2 px-4 rounded-lg hover:border-[#1570EF] focus:outline-[#84CAFF] focus:shadow-outline"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col mb-6">
+              <label htmlFor="email" className="text-[#344054] text-sm mb-1">
+                Email
               </label>
-              <input
+              <Field
+                name="email"
                 required
-                id="first_name"
-                placeholder="Enter your first name"
+                id="email"
+                placeholder="yourname@email.com"
                 className="placeholder:text-[#667085] placeholder:text-[15px] border border-[#D0D5DD] py-2 px-4 rounded-lg hover:border-[#1570EF] focus:outline-[#84CAFF] focus:shadow-outline"
               />
             </div>
-            <div className="flex flex-col mb-4 sm:mb-2 sm:w-[45%]">
-              <label htmlFor="last_name" className="text-[#344054] text-sm mb-1">
-                Last name
+            <div className="flex flex-col">
+              <label htmlFor="message" className="text-[#344054] text-sm mb-1">
+                Message
               </label>
-              <input
+              <Field
+                name="message"
+                as="textarea"
+                rows="5"
                 required
-                id="last_name"
-                placeholder="Enter your last name"
-                className="placeholder:text-[#667085] placeholder:text-[15px] border border-[#D0D5DD] py-2 px-4 rounded-lg hover:border-[#1570EF] focus:outline-[#84CAFF] focus:shadow-outline"
+                id="message"
+                placeholder="Send me a message and I'll reply you as soon as possible..."
+                className="text-gray-500 block placeholder:text-[#667085] placeholder:text-[15px] border border-[#D0D5DD] py-2 px-4 rounded-lg hover:border-[#1570EF] focus:outline-[#84CAFF] focus:shadow-outline"
               />
             </div>
-          </div>
-          <div className="flex flex-col mb-6">
-            <label htmlFor="email" className="text-[#344054] text-sm mb-1">
-              Email
-            </label>
-            <input
-              required
-              id="email"
-              placeholder="yourname@email.com"
-              className="placeholder:text-[#667085] placeholder:text-[15px] border border-[#D0D5DD] py-2 px-4 rounded-lg hover:border-[#1570EF] focus:outline-[#84CAFF] focus:shadow-outline"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="message" className="text-[#344054] text-sm mb-1">
-              Message
-            </label>
-            <textarea
-              rows="5"
-              required
-              id="message"
-              placeholder="Send me a message and I'll reply you as soon as possible..."
-              className="text-gray-500 block placeholder:text-[#667085] placeholder:text-[15px] border border-[#D0D5DD] py-2 px-4 rounded-lg hover:border-[#1570EF] focus:outline-[#84CAFF] focus:shadow-outline"
-            />
-          </div>
 
-          <div className="mt-8 mb-2 flex flex-row items-start">
+            <div className="mt-8 mb-2 flex flex-row items-start">
+              <Field
+                name="checkbox"
+                type="checkbox"
+                id="checkbox"
+                className="mr-[14px] mt-[5px] bg-gray-200 cursor-pointer border-[#D0D5DD] focus:ring-[#84CAFF] rounded"
+              />
+              <label htmlFor="checkbox" className="text-gray-600">
+                You agree to providing your data to Maverick who may contact
+                you.
+              </label>
+            </div>
             <input
-              required
-              id="checkbox"
-              type="checkbox"
-              className="text- mr-[14px] mt-[5px] bg-gray-200 cursor-pointer border-[#D0D5DD] focus:ring-[#84CAFF] rounded"
+              id="btn__submit"
+              value="Send Message"
+              type="submit"
+              className="text-sm text-white font-normal my-4 bg-[#1570EF] w-full text-center p-2 rounded-md cursor-pointer"
             />
-            <label htmlFor="checkbox" className="text-gray-600">
-              You agree to providing your data to Maverick who may contact you.
-            </label>
-          </div>
-          <input
-            id="btn__submit"
-            value="Send Message"
-            type="submit"
-            className="text-sm text-white font-normal my-4 bg-[#1570EF] w-full text-center p-2 rounded-md cursor-pointer"
-          />
-        </form>
+          </Form>
+        </Formik>
       </div>
       <Footer />
     </div>
